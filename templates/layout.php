23e1a644075a197b4a9d9f64be37748b212c497f
zolotuhina julia <asd295@yandex.ru>
2018-02-12 17:27:36 +0500

    для форм1

--- templates/layout.php
+++ templates/layout.php
@@ -7,7 +7,6 @@
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
-
 <body><!--class="overlay"-->
 <h1 class="visually-hidden">Дела в порядке</h1>
 
@@ -19,13 +18,12 @@
             </a>
 
             <div class="main-header__side">
-                <a class="main-header__side-item button button--plus" href="#">Добавить задачу</a>
+                <a class="main-header__side-item button button--plus" href="templates/modal-task.php">Добавить задачу</a>
 
                 <div class="main-header__side-item user-menu">
                     <div class="user-menu__image">
                         <img src="img/user-pic.jpg" width="40" height="40" alt="Пользователь">
                     </div>
-
                     <div class="user-menu__data">
                         <p>Константин</p>
 
